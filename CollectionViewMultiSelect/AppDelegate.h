d7955b36e8f0f6538caf9a9f22b9010b08e2e062
Baron_Nash <745352049@qq.com>
2018-06-20 11:52:55 +0800

    Initial Commit

---
+++ CollectionViewMultiSelect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewMultiSelect
+//
+//  Created by 水晶岛 on 2018/6/20.
+//  Copyright © 2018年 水晶岛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewMultiSelect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewMultiSelect
+//
+//  Created by 水晶岛 on 2018/6/20.
+//  Copyright © 2018年 水晶岛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
